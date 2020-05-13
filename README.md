1a4feff51ffb4c046ba9bfc6817d0f20790c8182
pany <pany@un-net.com>
2020-05-13 11:03:38 +0800

    引入sass&配置"baseUrl": "src"

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 # 在当前目录下创建一个react+ts的项目
 ## npx create-react-app . --typescript
+
+# .ts .tsx
+## .tsx是指支持标签的TS
+
+# @import-normalize;
+## @import-normalize是React自己发明的语法，它能自动添加一些全局css样式
+
+# 为了使import引入JS时更加方便（实现Vue中@符号的效果），需要在tsconfig.json中添加：
+## "baseUrl": "src",
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+  // React.StrictMode这个标签用来纠正代码错误的，删掉也并不影响程序
   <React.StrictMode>
     <App />
   </React.StrictMode>,
